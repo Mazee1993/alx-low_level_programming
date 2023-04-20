f13d5760c46fc02d5e8f92efee0b19fe1ea7a63a
Masello Monaheng <masellomonaheng@gmail.com>
2023-04-20 05:31:20 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#inclide<stdio.h>
+/**
+ * main -Entry points
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle
+");
+	return (0);
+}	
