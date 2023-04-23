2bb96cc15f0496de5cb4ff9c21745400e20db290
Masello Monaheng <masellomonaheng@gmail.com>
2023-04-23 10:03:07 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+	int d;
+	{
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+	}
+	{
+if (d != '9')
+putchar(',');
+putchar(' ');
+putchar('\n');
+	}
+return (0);
+}
+
