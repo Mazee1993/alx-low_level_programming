0f93ef20a9ef24884e8a56bf2db98f9d8613b1b8
Masello Monaheng <masellomonaheng@gmail.com>
2023-04-23 04:33:07 -0700

    Task 7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char low;
+
+	{
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
+	}
+return (0);
+}
