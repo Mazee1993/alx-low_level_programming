6d02b8d6a1e3bb2627d9f61080b863b4f64eda24
Masello Monaheng <masellomonaheng@gmail.com>
2023-04-23 04:06:11 -0700

    Task 5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0, followed by a new line
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
  * Return: 0
  */
 
