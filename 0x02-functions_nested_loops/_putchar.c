b2125e94afcc415d0c06e535fbd66372830d7cdb
Masello Monaheng <masellomonaheng@gmail.com>
2023-04-25 05:25:58 -0700

    Task Tasks 0. _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * _putchar - write a character c to stdout
+ * @C: The character to print
+ * Return: On success 1.
+ * On error. -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c)
+
+{
+return (write(1, &c, 1));
+}
+
+ 
