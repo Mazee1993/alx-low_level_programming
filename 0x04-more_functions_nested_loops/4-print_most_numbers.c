0fece650d0d3ec94c2ea8c05abc4eb1db7895474
Masello Monaheng <masellomonaheng@gmail.com>
2023-04-27 05:29:31 -0700

    Task 4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers -  prints the numbers, from 0 to 9,
+ * followed by a new line
  * Description: prints the number excluding 2 and 4
  * Return: The number 0 up to 9
  */
