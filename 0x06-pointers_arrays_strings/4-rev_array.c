97c15968412453e18bf8a4aaf2d55b2ab737abbd
Masello Monaheng <masellomonaheng@gmail.com>
2023-05-04 14:29:30 -0700

    Task 4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,13 +3,12 @@
 /**
  * reverse_arra -  function that reverses the content of an array of integers
  * @a: the array of integers to be reversed
- * @n: The number of elements in the array 
+ * @n: The number of elements in the array
  */
 
 void reverse_array(int *a, int n)
 {
 	int tmp, index;
-
 for (index = n - 1; index >= n / 2; index--)
 {
 tmp = a[n - 1 - index];
