e2cfbd3d61e7e69629e8ef39e2f57269cd7ff2a8
Masello Monaheng <masellomonaheng@gmail.com>
2023-05-10 07:15:08 -0700

    Task 6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,8 +12,8 @@ int check_prime(int n, int i);
 
 int is_prime_number(int n)
 {
-        return (check_prime(n, 1));
-}       
+return (check_prime(n, 1));
+{
 
 /**
  * check_prime - check if number is prime
@@ -24,11 +24,12 @@ int is_prime_number(int n)
  */
 
 int check_prime(int n, int i)
-{       
-        if (n <= 1)
-                return (0);
-        
-        if (n % i == 0 && i > 1)
+{
+
+if (n <= 1)
+return (0);
+
+if (n % i == 0 && i > 1)
 return (0);
 
 if ((n / i) < i)
